21d8c1949de6b101f41f5bf2d92055ea2a245b25
Kyle T Sit <ktsit@ieng6-202.ucsd.edu>
2016-05-04 21:54:03 -0700

    Hashtable done?

--- DictionaryHashtable.cpp
+++ DictionaryHashtable.cpp
@@ -1,18 +1,31 @@
 #include "util.hpp"
 #include "DictionaryHashtable.hpp"
+#include <unordered_set>
 
 /* Create a new Dictionary that uses a Hashset back end */
-DictionaryHashtable::DictionaryHashtable(){}
+DictionaryHashtable::DictionaryHashtable(){
+  hashTable = new std::unordered_set<std::string>();
+}
 
 /* Insert a word into the dictionary. */
 bool DictionaryHashtable::insert(std::string word)
 {
+  if( (hashTable->insert(word)).second ) {
+    return true;
+  }
+  return false;
 }
 
 /* Return true if word is in the dictionary, and false otherwise */
 bool DictionaryHashtable::find(std::string word) const
 {
+  if( hashTable->find(word) == hashTable->end() ) {
+    return false;
+  }
+  return true;
 }
 
 /* Destructor */
-DictionaryHashtable::~DictionaryHashtable(){}
+DictionaryHashtable::~DictionaryHashtable(){
+  delete hashTable;
+}
